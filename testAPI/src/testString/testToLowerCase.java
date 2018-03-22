648618ac4bb620318ad08d802e2b5c1f668ad614
李国建 <13144602916@163.com>
2018-03-22 19:33:15 +0800

    测试toLowerCase

---
+++ testAPI/src/testString/testToLowerCase.java
@@ -0,0 +1,14 @@
+package testString;
+
+//toLowerCase并不改变源字符串.
+public class testToLowerCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Hello World";
+		String s2 = s1.toLowerCase();
+		System.out.println("源字符串:" + s1);
+		System.out.println("转变后:" + s2);
+	}
+
+}
