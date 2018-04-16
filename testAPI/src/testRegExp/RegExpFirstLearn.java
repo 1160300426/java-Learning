848ffa67c96af0ac58760fb8467237034f9554b0
李国建 <13144602916@163.com>
2018-04-16 13:18:03 +0800

    测试RegExp 2.0

--- testAPI/src/testRegExp/RegExpFirstLearn.java
+++ testAPI/src/testRegExp/RegExpFirstLearn.java
@@ -7,15 +7,28 @@ public class RegExpFirstLearn {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String string = "Vertex = <¡°Server1¡±, ¡°Server¡±, <¡°192.168.1.2¡±>> ";
-		String pattern = "[a-zA-z1-9]";
+		String pattern = "¡°([a-zA-Z1-9]+)¡±,\\s+¡°([a-zA-Z1-9]+)¡±, <¡°([a-zA-Z1-9.]+)¡±>";
 		
 		Pattern r = Pattern.compile(pattern);
 		Matcher m = r.matcher(string);
 		if (m.find()) {
-			System.out.println("Found value: " + m.group(0) );
-	        System.out.println("Found value: " + m.group(1) );
-	        System.out.println("Found value: " + m.group(2) );
-	        System.out.println("Found value: " + m.group(3) ); 
+			System.out.println("Found value: " + m.group(0));
+	        System.out.println("Found value: " + m.group(1));
+	        System.out.println("Found value: " + m.group(2));
+	        System.out.println("Found value: " + m.group(3)); 
+		} else {
+			System.out.println("NO MATCH");
+		}
+		
+		
+		String string2 = "VertexType = ¡°Movie¡±, ¡°Actor¡±, ¡°Director¡± ";
+		String pattern2 = "([a-zA-Z]+)";
+		
+		Pattern r2 = Pattern.compile(pattern2);
+		Matcher m2 = r2.matcher(string2);
+		if (m2.find()) {
+			System.out.println("Found value2: " + m2.group(0));
+	        System.out.println("Found value2: " + m2.group(1));
 		} else {
 			System.out.println("NO MATCH");
 		}
