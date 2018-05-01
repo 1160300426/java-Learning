66cd891d0e4d7f6db751536dc99124ef0a016318
李国建 <13144602916@163.com>
2018-05-01 23:39:31 +0800

    close s

---
+++ testAPI/src/testSanner/testScannerInput.java
@@ -0,0 +1,18 @@
+package testSanner;
+import java.util.Scanner;
+
+public class testScannerInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);  
+	    System.out.println("ÇëÊäÈë×Ö·û´®£º");  
+	    while(true) {  
+	        String lin = s.nextLine();  
+	        if(lin.equals("exit")) break;  
+	        System.out.println(">>>" + lin);  
+	    }
+	    s.close();  
+	}
+
+}
