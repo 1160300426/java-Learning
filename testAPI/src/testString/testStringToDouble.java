8ae2bd70f1d1a7b62480c997057c8cecbfb8d137
李国建 <13144602916@163.com>
2018-05-16 20:07:55 +0800

    测试字符串转换double抛的异常

---
+++ testAPI/src/testString/testStringToDouble.java
@@ -0,0 +1,11 @@
+package testString;
+
+public class testStringToDouble {
+
+	public static void main(String[] args) {
+		String string = "lgj";
+		double double1 = Double.parseDouble(string);
+		System.out.println(double1);
+	}
+
+}
