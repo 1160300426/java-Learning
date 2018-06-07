d0bf5a0c3efa559f95b37dea91be1dedaf66bc96
李国建 <13144602916@163.com>
2018-06-07 20:01:12 +0800

    正则。。

--- testAPI/src/testRegExp/TestInter.java
+++ testAPI/src/testRegExp/TestInter.java
@@ -3,21 +3,17 @@ package testRegExp;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class testChar {
+public class TestInter {
 
 	public static void main(String[] args) {
-		
-		String string0 = "LiGuojian";
-		String pattern0 = "([G]+)";
+		String string0 = "2018-05-20 00:00:14";
+		String pattern0 = "(\\d{4}-\\d{2}-\\d{2})\\s+(\\d{2}:\\d{2}:\\d{2})";
 		
 		Pattern r0 = Pattern.compile(pattern0);
 		Matcher m0 = r0.matcher(string0);
 		if (m0.find()) {
 			System.out.println("Found : " + m0.group(1));
+			System.out.println("Found : " + m0.group(2));
 		}
-		
-		String s0 = "Poet-Walk";
-		System.out.println(s0.matches("\\w+"));
 	}
-
 }
